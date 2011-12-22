802f7d879c69bc56c9ef9c3ca8874cde11e88d72
Paul O'Connell <paul.t.oconnell@gmail.com>
2011-12-22 16:36:07 +0000

    Add juqery and a db loading service

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,11 @@
 class BootStrap {
 
     def init = { servletContext ->
-    }
+		def dbs = new ie.bhaa.LoadDBService()
+		dbs.rootDir = servletContext.getRealPath("/data/")
+		println "loading data... " + dbs.rootDir
+		dbs.load()
+    }	
     def destroy = {
     }
 }
