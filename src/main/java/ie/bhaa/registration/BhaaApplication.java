1fc3a0dd188189e6331f7282e905d4b341172093
emeraldjava <paul.t.oconnell@gmail.com>
2015-06-26 12:09:35 +0100

    Add basic maven spring files.

---
+++ src/main/java/ie/bhaa/registration/BhaaApplication.java
@@ -0,0 +1,12 @@
+package ie.bhaa.registration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BhaaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BhaaApplication.class, args);
+    }
+}
