263e94ca6dbb849bac55c701d66cffb947b1ff8e
emeraldjava <paul.t.oconnell@gmail.com>
2015-06-28 17:30:54 +0100

    Add spring.jpa.database-platform property

--- src/main/java/ie/bhaa/registration/BhaaApplication.java
+++ src/main/java/ie/bhaa/registration/BhaaApplication.java
@@ -6,49 +6,13 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-//@Configuration
-//@EnableAutoConfiguration
-//@ComponentScan
-
 @SpringBootApplication
 @EnableJpaRepositories("ie.bhaa.registration.racetec.repository")
 public class BhaaApplication extends SpringBootServletInitializer {
-//@SpringBootApplication
-//public class BhaaApplication {
-
-//    @Bean
-//    public DispatcherServlet dispatcherServlet() {
-//        return new DispatcherServlet();
-//    }
-//
-//    /**
-//     * Register dispatcherServlet programmatically
-//     *
-//     * @return ServletRegistrationBean
-//     */
-//    @Bean
-//    public ServletRegistrationBean dispatcherServletRegistration() {
-//        ServletRegistrationBean registration = new ServletRegistrationBean(
-//                dispatcherServlet(), "/*");
-//        registration
-//                .setName(DispatcherServletAutoConfiguration.DEFAULT_DISPATCHER_SERVLET_REGISTRATION_BEAN_NAME);
-//        return registration;
-//    }
-//
-////    @Override
-//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-//        return application.sources(BhaaApplication.class);
-//    }
-
 
     public static void main(String[] args) {
         SpringApplication.run(BhaaApplication.class, args);
     }
-
-//    @Override
-//    protected void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
-//        configurer.enable();
-//    }
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
