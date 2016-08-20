409a2c9ef4ace09a58de58a50d5195037f576fb3
Paul O'Connell <paul.t.oconnell@gmail.com>
2016-08-20 12:39:36 +0100

    Update the angular app.

--- registration/src/main/resources/static/app/app.module.js
+++ registration/src/main/resources/static/app/app.module.js
@@ -1,9 +1,9 @@
 /**
  * Created by pauloconnell on 26/06/15.
  */
-angular.module('bhaaWebApp', ['ui.bootstrap']);
+//angular.module('bhaaWebApp',['ui.bootstrap']);
 
-angular.module('bhaaWebApp').controller('TabsDemoCtrl', function ($scope, $window) {
+angular.module('app',[]).controller('TabsDemoCtrl', function ($scope, $window) {
     $scope.tabs = [
         { title:'Dynamic Title 1', content:'Dynamic content 1' },
         { title:'Dynamic Title 2', content:'Dynamic content 2', disabled: true }
@@ -11,14 +11,16 @@ angular.module('bhaaWebApp').controller('TabsDemoCtrl', function ($scope, $windo
 
     $scope.alertMe = function() {
         setTimeout(function() {
-            $window.alert('You\'ve selected the alert tab!');
+            $window.alert('You selected the alert tab!');
         });
     };
+}).controller('home', function($scope) {
+    $scope.greeting = {id: 'xxx', content: 'Hello World!'}
 });
 
 
 function Hello($scope, $http) {
-    $http.get('http://localhost:8080/rest').
+    $http.get('http://localhost:8080/api/runners').
         success(function(data) {
             $scope.greeting = data;
         });
