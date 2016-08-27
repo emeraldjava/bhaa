4c62eef59d4eed2fc8f442b3fdc66e3078ad0760
Paul O'Connell <paul.t.oconnell@gmail.com>
2016-08-27 18:15:49 +0100

    Add the typeahead component.

--- registration/src/main/resources/static/app/app.module.js
+++ registration/src/main/resources/static/app/app.module.js
@@ -3,7 +3,17 @@
  */
 //angular.module('bhaaWebApp',['ui.bootstrap']);
 
-angular.module('app',['ui.bootstrap'])
+angular.module('app',['ui.bootstrap','service','ngRoute'])
+    .controller('memberTypeaheadCtrl', function($scope, $http) {
+        var _selected;
+
+        //$http.get('./js/bhaa_members.js').success(function(data) {
+        //    $scope.runners = data;
+        //});
+        //$scope.runners=[{"id":"dard":"19"}];
+
+        $scope.runners=window.bhaa_members;
+    })
     .controller('tabsDemoCtrl', function ($scope, $window) {
         $scope.tabs = [
             { title:'Dynamic Title 1', content:'Dynamic content 1' },
@@ -19,12 +29,23 @@ angular.module('app',['ui.bootstrap'])
     .controller('home', function($scope) {
         $scope.greeting = {id: 'xxx', content: 'Hello World!'}
     })
-;
+    .controller('InvoiceController', ['currencyConverter', function(currencyConverter) {
+        this.qty = 1;
+        this.cost = 2;
+        this.inCurr = 'EUR';
+        this.currencies = currencyConverter.currencies;
 
+        this.total = function total(outCurr) {
+            return currencyConverter.convert(this.qty * this.cost, this.inCurr, outCurr);
+        };
+        this.pay = function pay() {
+            window.alert("Thanks!");
+        };
+    }]);
 
-function Hello($scope, $http) {
-    $http.get('http://localhost:8080/api/runners').
-        success(function(data) {
-            $scope.greeting = data;
-        });
-}
+//function Hello($scope, $http) {
+//    $http.get('http://localhost:8080/api/runners').
+//        success(function(data) {
+//            $scope.greeting = data;
+//        });
+//}
