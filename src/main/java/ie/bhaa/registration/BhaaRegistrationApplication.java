53e457ff96ff10e4bc1565fd7a07f659bc2875e2
paul.oconnell <paul.oconnell@bearingpoint.com>
2017-01-26 11:12:44 +0000

    exclude the DataSourceAutoConfiguration config.

--- src/main/java/ie/bhaa/registration/BhaaRegistrationApplication.java
+++ src/main/java/ie/bhaa/registration/BhaaRegistrationApplication.java
@@ -1,17 +1,16 @@
 package ie.bhaa.registration;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BhaaRegistrationApplication extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(BhaaRegistrationApplication.class);
-    }
+// http://stackoverflow.com/questions/24074749/spring-boot-cannot-determine-embedded-database-driver-class-for-database-type
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+public class BhaaRegistrationApplication { //extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(BhaaRegistrationApplication.class, args);
