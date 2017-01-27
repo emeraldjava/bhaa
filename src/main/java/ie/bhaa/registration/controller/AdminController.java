e1698cbf4aa2e41f328f10700b38486ec79d971d
paul.oconnell <paul.oconnell@bearingpoint.com>
2017-01-27 14:45:08 +0000

    Update file writing logic to use the date

--- src/main/java/ie/bhaa/registration/controller/AdminController.java
+++ src/main/java/ie/bhaa/registration/controller/AdminController.java
@@ -1,19 +1,14 @@
 package ie.bhaa.registration.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import ie.bhaa.registration.domain.Runner;
 import ie.bhaa.registration.enumeration.Page;
+import ie.bhaa.registration.service.MembershipFileServiceBean;
 import org.apache.log4j.Logger;
-import org.springframework.context.ResourceLoaderAware;
-import org.springframework.core.io.ResourceLoader;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.io.File;
-import java.util.List;
 import java.util.Map;
 
 /**
@@ -21,77 +16,43 @@ import java.util.Map;
  */
 @Controller
 @RequestMapping("/admin")
-public class AdminController implements ResourceLoaderAware {
+public class AdminController {
 
     private Logger logger = Logger.getLogger(AdminController.class);
-    private String DYNAMIC_FOLDER = "/dynamic";
-    private String MEMBERS_JSON = "members-list.json";
 
-    private File folder;
-    private ResourceLoader resourceLoader;
+    @Autowired
+    private MembershipFileServiceBean membershipFileService;
 
     @RequestMapping("/")
     public String index(Map<String, Object> model) {
         model.put("file", "Filename.json");
-
-
-
-        model.put("menu",Page.values());
+        model.put("menu", Page.values());
         return "member";
     }
 
     @RequestMapping("/loadFile")
     public String loadFile(Map<String, Object> model) throws Exception {
         logger.info("loadFile");
-        RestTemplate restTemplate = new RestTemplate();
-        String quote = restTemplate.getForObject("http://bhaaie/bhaawp?bhaa-registration=xx&bhaa-registration-token=bhaa&limit=20&status=M", String.class);
-        System.out.print(quote);
-        logger.info(quote);
-
-        //writeFile(getRunners());
-        model.put("menu",Page.values());
+        membershipFileService.reloadMembershipFile();
+        model.put("menu", Page.values());
         logger.info("loadFile");
         return "/admin";
     }
 
     @RequestMapping("/clearallfiles")
     public String clearallfiles(Map<String, Object> model) {
-        model.put("menu",Page.values());
+        model.put("menu", Page.values());
+        membershipFileService.clearMembershipFile();
         logger.info("clearallfiles");
         return "/admin";
     }
-
-    private String writeFile(List<Runner> list) throws Exception {
-        File folder = this.resourceLoader.getResource("file:dynamic").getFile();
-        logger.info(folder.getAbsolutePath());
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.writeValue(new File(folder,MEMBERS_JSON), list);
-        return MEMBERS_JSON;
-    }
-
-//    private List<Runner> getRunners() {
-//        List<Runner> list = new ArrayList<Runner>();
-//        list.add(new Runner(1000l,"Paul","B1"));
-//        list.add(new Runner(2000l,"Pat","B2"));
-//        list.add(new Runner(3000l,"Paaut","B3"));
-//        list.add(new Runner(4000l,"Jacobe","B4"));
-//        list.add(new Runner(1100l,"xPaul","B1"));
-//        list.add(new Runner(2100l,"xPat","B2"));
-//        list.add(new Runner(3100l,"xPaaut","B3"));
-//        list.add(new Runner(4100l,"xJacobe","B4"));
-//        return list;
-//    }
 
     @ExceptionHandler(Exception.class)
     public ModelAndView handleAllException(Exception ex) {
         ModelAndView model = new ModelAndView("error/generic_error");
         model.addObject("errMsg", ex.getMessage());
         logger.error(ex);
+        ex.printStackTrace();
         return model;
-    }
-
-    @Override
-    public void setResourceLoader(ResourceLoader resourceLoader) {
-        this.resourceLoader=resourceLoader;
     }
 }
--- src/main/java/ie/bhaa/registration/service/MembershipFileServiceBean.java
+++ src/main/java/ie/bhaa/registration/service/MembershipFileServiceBean.java
@@ -1,21 +1,52 @@
 package ie.bhaa.registration.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import ie.bhaa.registration.dto.MembershipData;
+import ie.bhaa.registration.dto.Runner;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.ResourceLoaderAware;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.PostConstruct;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
 /**
  * Created by pauloconnell on 27/01/17.
  */
+@Component
 @ConfigurationProperties
-public class MembershipFileServiceBean implements MembershipFileService {
+public class MembershipFileServiceBean implements MembershipFileService, ResourceLoaderAware {
 
     private Logger logger = Logger.getLogger(MembershipFileServiceBean.class);
 
     @Value("${url}")
     private String url;
+
+    private String FILE_DYNAMIC = "file:dynamic";
+    private String MEMBERS_JSON = "members-list.json";
+
+    private ResourceLoader resourceLoader;
+    private File folder;
+
+    @PostConstruct
+    public void loadFolder() {
+        try {
+            logger.info("MembershipFileServiceBean - init");
+            this.folder = this.resourceLoader.getResource(FILE_DYNAMIC).getFile();
+            logger.info(folder.getAbsolutePath());
+        }catch(Exception e){
+            logger.error(e);
+        }
+    }
 
     public String getUrl(){
         return url;
@@ -24,12 +55,6 @@ public class MembershipFileServiceBean implements MembershipFileService {
     public void setUrl(){
         this.url = url;
     }
-
-//    private String DYNAMIC_FOLDER = "/dynamic";
-//    private String MEMBERS_JSON = "members-list.json";
-//
-//    private File folder;
-//    private ResourceLoader resourceLoader;
 
     @Override
     public String getMembershipFile() {
@@ -42,13 +67,46 @@ public class MembershipFileServiceBean implements MembershipFileService {
         RestTemplate restTemplate = new RestTemplate();
         MembershipData membershipData = restTemplate.getForObject(getUrl(), MembershipData.class);
         //System.out.print(quote);
+        //loadFolder();
         logger.info(membershipData.getCount());
         logger.info(membershipData.getRunners().size());
-
+        try {
+            writeFile(membershipData.getRunners(),membershipData.getDate());
+        } catch(Exception e){
+            logger.error(e);
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void clearMembershipFile() {
+        try {
+            //loadFolder();
+            Files.walk(Paths.get(folder.toURI()))
+                    .filter(Files::isRegularFile)
+                    //.filter(Path::toString().endswith(".json"))
+                    .map(Path::toFile)
+                    .forEach(File::delete);
+        } catch(IOException ioe) {
+            logger.error(ioe);
+            ioe.printStackTrace();
+        }
+    }
 
+    private String writeFile(List<Runner> list,String date) throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        File file = new File(this.folder,"members-list-"+date+".json");
+        mapper.writeValue(file,list);
+        logger.info(file.getAbsolutePath());
+        return MEMBERS_JSON;
+    }
+
+    public ResourceLoader getResourceLoader() {
+        return resourceLoader;
+    }
+
+    @Override
+    public void setResourceLoader(ResourceLoader resourceLoader) {
+        this.resourceLoader = resourceLoader;
     }
 }
